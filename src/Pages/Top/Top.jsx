02611f7d81ef790710d2fec759747996d9995159
Richard-Dev <richardbonilla.dev@gmail.com>
2025-06-18 20:16:07 -0600

    Avance 18/06

---
+++ src/Pages/Top/Top.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Top = () => {
+  return (
+    <div className='top__main d-flex'>
+        <div className="icon_info">
+            <img src=''/>
+            <span></span>
+        </div>
+        <divider />
+        <div className="icon_info">
+            <img src=''/>
+            <span></span>
+        </div>
+        <divider />
+        <div className="icon_info">
+            <img src=''/>
+            <span></span>
+        </div>
+        <divider />
+        <div className="social-icons">
+            <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+        </div>
+    </div>
+  )
+}
